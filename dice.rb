e27250ce8ee9bba5e739450aaedfc8cabda72523
jbahena657 <143520555+jbahena657@users.noreply.github.com>
2024-01-18 15:36:00 +0000

    2d6 no work

--- dice.rb
+++ dice.rb
@@ -1,6 +1,15 @@
 # /dice.rb
 
 require "sinatra"
+require "sinatra/reloader"
+
 get("/") do
   "Hello World"
+end
+
+get("/zebra") do
+  "Zebra is black white like the ying-yang"
+end
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
